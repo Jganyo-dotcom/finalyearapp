0d0a1a1747bc5f668b840bdba2053c9285960c20
jganyo-dotcom <elikemejay@gmail.com>
2025-06-09 13:37:31 +0000

    'topicsalmostdone'

---
+++ store/static/store/store.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('ready')
+    document.querySelectorAll('button').forEach (function(button){
+        button.onclick = function(){
+            const area = this.dataset.info
+             document.querySelector('.info').innerHTML = `<h1>Loading please wait ...<h1>`
+            fetch(`https://dataverse.harvard.edu/api/search?q=${area}&type=dataset`)
+    .then(response => response.json())
+    .then(data =>{
+        let info = data.data.items
+        document.querySelector('.info').innerHTML = ''
+       
+      
+        for ( let item of info){
+            
+             const name = item.name
+             const writter = item.authors || []
+             let writter_1 = writter[0] || 'unknown'
+             let writter_2 = writter[1] || '';
+             let writter_3 = writter[2] || '';
+             const link = item.url
+             
+               newdiv = document.createElement('div')
+               
+    newdiv.innerHTML= `
+    Title : ${name}<br>
+    link : <a href ='${link}' target = '_blank'>${link}</a><br>
+    authors:${writter_1},   ${writter_2} ,${writter_3}`
+
+    document.querySelector('.info').appendChild(newdiv)
+            
+        }
+        
+    })
+    .catch(error =>
+        console.error(
+     error
+        )
+    )
+
+    
+        }
+        
+    })
+    
+})
